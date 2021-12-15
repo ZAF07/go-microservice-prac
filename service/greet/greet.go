9d31a446f0be5949f8ca0d8fdccf57c8c3b55ac8
ZAF07 <zaffere07@gmail.com>
2021-12-15 22:43:39 +0800

    test token

--- service/greet/greet.go
+++ service/greet/greet.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/ZAF07/go-microservice-prac/constants"
+	"github.com/ZAF07/go-microservice-prac/manager"
 	"github.com/ZAF07/go-microservice-prac/proto/rpc"
 )
 
@@ -27,6 +28,15 @@ func NewGreetService() *Greet {
 func (s *Greet) Greetings(ctx context.Context, req *rpc.Greeting) (resp *rpc.Reply, err error) {
 	fmt.Println("RECIEVED A REQUEST !!")
 	fmt.Printf("CONTEXT RECEIVED : %+v\n", ctx.Value(constants.CtxKey("name")))
+	fmt.Println("SAVING FILE...")
+	saveErr := manager.Save()
+	if saveErr != nil {
+		fmt.Println("SAVE ERROR")
+	} else {
+		fmt.Println("Save success")
+	}
+// this
+	manager.Shout()
 	resp = &rpc.Reply{
 		Reply: "Yes hello",
 	}
